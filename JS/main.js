d6a0073381e38e72d5fa6124cff0ac88f6246940
Moh7mmedEhab <mohammed.ehab.23062@gmail.com>
2024-10-05 18:55:22 +0300

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,5 @@
+let header = document.querySelector("header");
+
 let links = document.querySelectorAll("nav ul li button");
 
 let startButton = document.querySelector("#about-btn");
@@ -6,13 +8,59 @@ let aboutSection = document.querySelector("#about-me");
 
 let mail = document.querySelector(".mail");
 
+let whatsapp = document.querySelector(".whatsapp");
+
 let skillsSection = document.querySelector("#skills");
 
 let progs = document.querySelectorAll(".progress");
 
+let listContainer = document.querySelector("#list-container");
+
+let slideContainer = document.querySelector("#links");
+
+let spanOne = document.querySelector(".one");
+
+let spanTwo = document.querySelector(".two");
+
+let spanThree = document.querySelector(".three");
+
+let listOpen = false;
+
+let porjects = document.querySelectorAll(".project");
+
+window.onload = () => {
+
+    if (window.innerWidth <= 700) {
+
+        header.style.height = `${window.innerHeight - (window.innerHeight - document.documentElement.clientHeight)}px`;
+    
+    };
+
+};
+
+addEventListener('resize', () => {
+
+    if (window.innerWidth <= 700) {
+
+        header.style.height = `${window.innerHeight - (window.innerHeight - document.documentElement.clientHeight)}px`;
+    
+    } else {
+
+        header.style.height = "100vh";
+
+    };
+
+});
+
 links.forEach(link => {
 
     link.onclick = () => {
+
+        if (window.innerWidth <= 700) {
+
+            listContainer.click();
+
+        }
 
         window.scrollTo({
 
@@ -26,7 +74,53 @@ links.forEach(link => {
 
     }
 
-})
+});
+
+listContainer.onclick = () => {
+
+    if (!listOpen) {
+
+        spanOne.style.position = "absolute";
+
+        spanOne.style.backgroundColor = "red";
+
+        spanOne.style.transform = "rotate(45deg)";
+
+        spanTwo.style.opacity = "0";
+
+        spanThree.style.position = "absolute";
+
+        spanThree.style.backgroundColor = "red";
+
+        spanThree.style.transform = "rotate(-45deg)";
+
+        slideContainer.style.top = "0";
+
+        listOpen = true;
+
+    } else {
+
+        spanOne.style.position = "static";
+
+        spanOne.style.backgroundColor = "white";
+
+        spanOne.style.transform = "rotate(0deg)";
+
+        spanTwo.style.opacity = "1";
+
+        spanThree.style.position = "static";
+
+        spanThree.style.backgroundColor = "white";
+
+        spanThree.style.transform = "rotate(0deg)";
+
+        slideContainer.style.top = "-100%";
+
+        listOpen = false;
+
+    }
+
+};
 
 startButton.onclick = () => {
 
@@ -42,13 +136,19 @@ startButton.onclick = () => {
 
     });
 
-}
+};
+
+whatsapp.onclick = () => {
+
+    window.location.href = "https://wa.me/+201062089220?text=hello+developer.";
+
+};
 
 mail.onclick = () => {
 
     window.location.href = "mailto:Mohammed.Ehab.23062@gmail.com";
 
-}
+};
 
 window.onscroll = () => {
 
@@ -58,8 +158,20 @@ window.onscroll = () => {
 
             prog.style.width = prog.dataset.progress;
         
-        })
+        });
 
-    }
+    };
 
-}
+};
+
+porjects.forEach(project => {
+
+    project.onclick = () => {
+
+        window.location.href = project.dataset.location;
+
+    };
+
+});
+
+links.stylesheet = "display:none;"
